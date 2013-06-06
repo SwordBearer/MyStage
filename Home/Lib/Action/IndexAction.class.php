0eceabd539c800d42aee6f7cf45565efd1deffc8
SwordBearer Tang <tangyuchun@gmail.com>
2013-06-06 12:41:35 +0800

    局部修改
    局部修改

--- Home/Lib/Action/IndexAction.class.php
+++ Home/Lib/Action/IndexAction.class.php
@@ -2,6 +2,6 @@
 class IndexAction extends Action {
     public function index(){
     	$this->display();
-    	//$this->redirect("__GROUP__/Blog/index");
+    	$this->redirect("__GROUP__/Blog/index");
 	}
 }
