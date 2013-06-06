23f0c1f16e819fd934640561e1382ef1a70ea775
SwordBearer Tang <tangyuchun@gmail.com>
2013-06-06 17:22:10 +0800

    修改博客详情页面

--- Home/Lib/Model/BlogModel.class.php
+++ Home/Lib/Model/BlogModel.class.php
@@ -30,7 +30,7 @@ class BlogModel extends Model{
 	}
 
 	public function getRecentBlogs(){
-		$sql="SELECT blog.id,blog.title,blog.inputtime FROM mystage_blog AS blog WHERE blog.status=1 ORDER BY inputtime DESC limit 10 ";
+		$sql="SELECT blog.id,blog.title,blog.inputtime FROM mystage_blog AS blog WHERE blog.status=1 ORDER BY inputtime DESC limit 5 ";
 		$result=$this->query($sql);
 		return $result;
 	}
--- Home/Runtime/Cache/cfdd517e0d671e92ae3da660d3de2eb0.php
+++ Home/Runtime/Cache/cfdd517e0d671e92ae3da660d3de2eb0.php
@@ -15,7 +15,7 @@
 <body>
 <div class="nav">
 	<div class="nav-inner">
-		<a class="brand" href="<?php echo U(index);?>">SwordBearer's Lab</a>
+		<a class="brand" href="../Index/index">SwordBearer's Lab</a>
 		<ul class="nav-menu">
 			<li></li>
 			<li>
@@ -42,48 +42,38 @@
 <div class="main_content">
 	<!--右侧栏-->
 	<div  class="rightSide baseDiv">
-		<!-- <div class="sideDiv">
-		<div class="sidebarTitle">W T F ?</div>
-		<ul class="sidebarMenu">
-			<li>日访问量：237</li>
-			<li>总访问量：3743</li>
-			<li>原创(231) | 转载(34) | 翻译(9)</li>
-		</ul>
+		<!-- 微博秀 -->
+		<iframe width="100%" height="600" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=600&fansRow=2&ptype=1&speed=0&skin=9&isTitle=0&noborder=0&isWeibo=1&isFans=1&uid=2407334984&verifier=ec0391a3&dpc=1"></iframe>
 	</div>
-	<hr/>
-	-->
-	<!-- 微博秀 -->
-	<iframe width="100%" height="600" class="share_self"  frameborder="0" scrolling="no" src="http://widget.weibo.com/weiboshow/index.php?language=&width=0&height=600&fansRow=2&ptype=1&speed=0&skin=9&isTitle=0&noborder=0&isWeibo=1&isFans=1&uid=2407334984&verifier=ec0391a3&dpc=1"></iframe>
-</div>
-<!-- end 右侧栏 -->
-<!-- 主要内容 -->
-<div class="feedDiv baseDiv">
-	<?php if(is_array($allBlogs)): $i = 0; $__LIST__ = $allBlogs;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$blog): $mod = ($i % 2 );++$i;?><div class="feed">
-			<div class="head">
-				<!-- <span class="date"><?php echo ($blog["inputtime"]); ?></span>
-			-->
-			<a href="<?php echo U(blog_details);?>/blog/<?php echo ($blog["id"]); ?>" target="_blank">
-				<?php if($blog['catid'] == 1): ?><img src="__PUBLIC__/res/img/ico_monkey.png" />
-					<?php elseif($blog['catid'] == 2 ): ?>
-					<img src="__PUBLIC__/res/img/ico_essay.png" />
-					<?php else: ?>
-					<img src="__PUBLIC__/res/img/ico_enshrine.png" /><?php endif; ?>
-				<?php echo ($blog["title"]); ?>
-			</a>
-		</div>
-		<!-- <div class="body"><?php echo ($blog["content"]); ?></div>
-	-->
-	<div class="foot">
-		<span class="topic">专栏:<?php echo ($blog["topicname"]); ?></span>
-		&nbsp;&nbsp;
+	<!-- end 右侧栏 -->
+	<!-- 主要内容 -->
+	<div class="feedDiv baseDiv">
+		<?php if(is_array($allBlogs)): $i = 0; $__LIST__ = $allBlogs;if( count($__LIST__)==0 ) : echo "" ;else: foreach($__LIST__ as $key=>$blog): $mod = ($i % 2 );++$i;?><div class="feed">
+				<div class="head">
+					<!-- <span class="date"><?php echo ($blog["inputtime"]); ?></span>
+				-->
+				<a href="<?php echo U(blog_details);?>/blog/<?php echo ($blog["id"]); ?>" target="_blank">
+					<?php if($blog['catid'] == 1): ?><img src="__PUBLIC__/res/img/ico_monkey.png" />
+						<?php elseif($blog['catid'] == 2 ): ?>
+						<img src="__PUBLIC__/res/img/ico_essay.png" />
+						<?php else: ?>
+						<img src="__PUBLIC__/res/img/ico_enshrine.png" /><?php endif; ?>
+					<?php echo ($blog["title"]); ?>
+				</a>
+			</div>
+			<!-- <div class="body"><?php echo ($blog["content"]); ?></div>
+		-->
+		<div class="foot">
+			<span class="topic">专栏:<?php echo ($blog["topicname"]); ?></span>
+			&nbsp;&nbsp;
 		阅读(<?php echo ($blog["readcount"]); ?>)&nbsp;&nbsp;
-		<a href="<?php echo U(blog_details);?>/blog/<?php echo ($blog["id"]); ?>#commentTitle" target="_blank">评论(<?php echo ($blog["commentcount"]); ?>)</a>
-</div>
-</div><?php endforeach; endif; else: echo "" ;endif; ?>
+			<a href="<?php echo U(blog_details);?>/blog/<?php echo ($blog["id"]); ?>#commentTitle" target="_blank">评论(<?php echo ($blog["commentcount"]); ?>)</a>
+		</div>
+	</div><?php endforeach; endif; else: echo "" ;endif; ?>
 </div>
 <div id="gototop" style="display:none;">
 <a id="gototop_hint" href="#" title="回到顶部">
-<img src="__PUBLIC__/res/img/gototop.png" alt="TOP" />
+	<img src="__PUBLIC__/res/img/gototop.png" alt="TOP" />
 </a>
 </div>
 <script type="text/javascript">
